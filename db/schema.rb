0abfd89c71f1da324b072baa96530d08d27e3646
Fernando Antelo <fjantelo@gmail.com>
2022-11-30 17:55:44 -0500

    Adds Follower model and followers controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_221738) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_225421) do
+  create_table "followers", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "follower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", force: :cascade do |t|
     t.string "text"
     t.integer "user_id"
