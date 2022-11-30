8e464c0ac292819816f418fa7fa9700109f01f05
Fernando Antelo <fjantelo@gmail.com>
2022-11-30 17:58:34 -0500

    Adds Retweet model and retweets controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_225630) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_225757) do
   create_table "followers", force: :cascade do |t|
     t.integer "user_id"
     t.integer "follower_id"
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_225630) do
   end
 
   create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "retweets", force: :cascade do |t|
     t.integer "user_id"
     t.integer "tweet_id"
     t.datetime "created_at", null: false
