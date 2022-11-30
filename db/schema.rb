f7a9ce64fcbbc99c9f3b3750c2df3c05192106fa
Fernando Antelo <fjantelo@gmail.com>
2022-11-30 17:57:10 -0500

    Adds Like model and likes controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_225421) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_225630) do
   create_table "followers", force: :cascade do |t|
     t.integer "user_id"
     t.integer "follower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
