3e099208d0b4457d2e3b8fa2e3e2abc6217895a6
Fernando Antelo <fjantelo@gmail.com>
2022-12-01 20:37:55 -0500

    Adds create and destroy methods to tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,19 @@
 class TweetsController < ApplicationController
+  def create
+    tweet = Tweet.new(
+      user_id: current_user.id,
+      text: params[:text],
+    )
+    if tweet.save
+      render json: { message: "Tweet created successfully" }, status: :created
+    else
+      render json: { errors: tweet.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy # possibly mark tweets as deleted instead of actually deleting them?
+    tweet = Tweet.find_by(id: params[:id])
+    tweet.destroy
+    render json: { "message": "Tweet successfully deleted." }
+  end
 end
