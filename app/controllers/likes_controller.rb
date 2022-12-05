1fb4300ab5b4822b0a242d15b640c8b37a103ad9
Fernando Antelo <fjantelo@gmail.com>
2022-12-05 13:45:31 -0500

    Adds create and destroy methods to likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+  def create
+    like = Like.new(
+      user_id: current_user.id,
+      tweet_id: params[tweet_id],
+    )
+    if like.save
+      render json: { message: "Like created successfully" }, status: :created
+    else
+      render json: { errors: like.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    like = Like.find_by(id: params[:id])
+    like.destroy
+    render json: { "message": "like successfully deleted." }
+  end
 end
